c81f964ed0f932d7d40e61287efc07836ac66073
Petrut <ivanoiu.petrut27@yahoo.com>
2023-11-10 19:36:47 +0200

    feat: add all views for all algorithms

--- src/main/renderer/rendererShannon.js
+++ src/main/renderer/rendererShannon.js
@@ -6,12 +6,9 @@ const { shannonFanoCoding, writeShanonFile, decodeShannonFile } =
 
 const { clearBitReader } = window.electronAPI.require("../shared/bitReader");
 
-const { writeNBits } = window.electronAPI.require("../shared/bitWritter.js");
-
 const selectedFile = document.querySelector(".selected-file"),
   encryptButton = document.querySelector("#encryptButton"),
   decryptButton = document.querySelector("#decryptButton"),
-  testButton = document.querySelector("#testButton"),
   fileInput = document.querySelector("#fileInput");
 
 let filePath = "";
@@ -52,18 +49,4 @@ decryptButton.addEventListener("click", async () => {
 fileInput.addEventListener("change", (event) => {
   filePath = event.target.files[0].path;
   selectedFile.innerHTML = filePath;
-});
-
-testButton.addEventListener("click", () => {
-  const length = 32;
-  const bits1 = [1, 1, 1, 1, 1, 1, 1, 1];
-  const bits2 = [0, 1, 0, 1, 1, 0, 0, 0];
-
-  for (let i = 0; i < 50; i++) {
-    if (i % 2 === 0) {
-      writeNBits("C:\\Personal\\test1", bits1, length);
-    } else {
-      writeNBits("C:\\Personal\\test1", bits2, length);
-    }
-  }
 });
